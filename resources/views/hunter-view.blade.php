483ee47ff639ec93cd6bd59162cb423be7865563
abenbyy <andrbenaya@gmail.com>
2020-09-30 16:38:27 +0700

    Minor fix on view

--- resources/views/hunter-view.blade.php
+++ resources/views/hunter-view.blade.php
@@ -1,7 +1,7 @@
 <div>
     <ul>
         @foreach ($hunters as $hunter)
-            <li>Name: {{$hunter->name}} - Gender: {{$hunter->gender}} </li>    
+            <li>{ Name: {{$hunter->name}} - Gender: {{$hunter->gender}} }</li>    
         @endforeach
     </ul>
 </div>
